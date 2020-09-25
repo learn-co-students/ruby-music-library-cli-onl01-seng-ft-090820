cd8ac185001998c3d820f731e4a465eac15fdf52
csdj92 <csdj92@gmail.com>
2020-09-25 12:30:15 -0400

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,45 @@
+class Genre
+    attr_accessor :name
+    extend Concerns::Findable
+    @@all = []
+    
+    def initialize(name)
+        @name = name
+        @songs = []
+    end
+
+    def self.all
+        @@all        
+    end
+
+    def self.destroy_all
+        @@all.clear        
+    end
+
+    def save
+        @@all << self      
+    end
+
+    def self.create(genre)
+        genre = self.new(genre)
+        genre.save
+        genre
+    end
+    
+    def songs
+        @songs
+    end
+
+    def artists
+        @array = []
+        @songs.each do |song|
+            if @array.include?(song.artist)
+                nil
+            else
+                @array << song.artist
+            end
+        end
+        @array        
+    end
+
+end
